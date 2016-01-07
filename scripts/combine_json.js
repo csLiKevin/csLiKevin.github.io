66e374c8d61fe5d59ee1b949e00383cfa86bbfa2
csLiKevin <csLiKevin@users.noreply.github.com>
2016-01-06 23:30:42 -0500

    Removed the awards section from the web version.

--- scripts/combine_json.js
+++ scripts/combine_json.js
@@ -3,5 +3,6 @@ var extend = require("extend");
 
 var baseJson = JSON.parse(fs.readFileSync("input/base.json", "utf8"));
 var contactInfo = JSON.parse(fs.readFileSync("input/contact_info.json", "utf8"));
+var awards = JSON.parse(fs.readFileSync("input/awards.json", "utf8"));
 
-fs.writeFileSync("output/resume.json", JSON.stringify(extend(true, baseJson, contactInfo), null, 2));
+fs.writeFileSync("output/resume.json", JSON.stringify(extend(true, baseJson, contactInfo, awards), null, 2));
