2a0a1d60f56ad01cbb3bc17ef804e9192cba025c
csLiKevin <csLiKevin@users.noreply.github.com>
2022-09-22 22:17:04 -0400

    Swap Bulma with Tailwind and remove Svelte.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte(), compress()],
+    integrations: [compress(), tailwind()],
 });
