21136c2cc16522f3b3193f1bb29d4af03d830656
csLiKevin <csLiKevin@users.noreply.github.com>
2022-09-22 22:41:39 -0400

    Sort imports.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
-import compress from "astro-compress";
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
