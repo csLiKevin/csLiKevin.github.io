f5056610e331a81e0971ce7e6cf9d109a4ca8343
csLiKevin <csLiKevin@users.noreply.github.com>
2025-05-17 19:58:14 +0000

    Upgrade tailwind to 4.1.7 and daisyui to 5.0.35.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import tailwind from "@astrojs/tailwind";
 import compress from "@playform/compress";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), compress()],
+    integrations: [compress()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
